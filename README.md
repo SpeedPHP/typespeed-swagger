c2ccc385408a2636232d4486ff41a34cf0884898
speedphp <jake@speedphp.com>
2023-06-13 15:15:25 +0800

    created project

---
+++ README.md
@@ -0,0 +1,30 @@
+# typespeed-swagger 
+
+[![typescript](https://badgen.net/badge/icon/TypeScript?icon=typescript&label)](https://www.npmjs.com/package/typespeed)
+[![license](https://badgen.net/github/license/speedphp/typespeed)](https://github.com/SpeedPHP/typespeed/blob/main/LICENSE)
+
+**The Swagger plugin for the TypeSpeed framework.**
+
+### Install
+
+```
+npm install typespeed-swagger
+```
+
+### Usage
+
+1. Modify the imports of the following decorators in the original TypeSpeed project and replace them with the path from typespeed-swagger. The decorators that need to be replaced are: `@getMapping`、`@postMapping`、`@requestMapping`、`@reqBody`、`@reqQuery`、`@reqForm`.
+2. The three *Mapping decorators have a second optional parameter. If the response object is a Promise, please put the return type in the second parameter.
+3. Please put the `SwaggerMiddleware` into TypeSpeed's middleware list so that it can support Swagger web page access.
+
+### Link
+
+- **TypeSpeed Framework** <https://www.npmjs.com/package/typespeed>
+
+- **Swagger UI** <https://swagger.io>
+
+- **typescript-rtti** <https://www.npmjs.com/package/typescript-rtti>
+
+### License
+
+- [MIT](LICENSE) © speedphp
---
+++ test.ts
@@ -0,0 +1,80 @@
+import 'reflect-metadata';
+import { reflect, ReflectedFunction } from 'typescript-rtti';
+const methodsOne = new Map();
+
+class DataB{
+	constructor(public age: number, public cc: DataC){}
+}
+
+class DataC{
+	constructor(public bb: number){}
+}
+
+class DataA {
+	constructor(public id: number, public name: string, public dataB: DataB, public cc: DataC){}
+}
+
+class Page {
+
+	@responseMethod()
+	public getDataA(aa: number): DataA {
+		return new DataA(12, "zzz", new DataB(1, new DataC(2)), new DataC(3));
+	}
+
+	@responseMethod(DataA)
+	public async getDataAsync(@paramOne("data") ccc: DataA): Promise<DataA> {
+		return Promise.resolve(new DataA(12, "zzz", new DataB(2, new DataC(3)), new DataC(3)));
+	}
+
+}
+// 和 GetMapping 一起用，有第二个参数可选，输入参数就是返回对象的类型
+function responseMethod(clazz?) {
+	return (target: any, propertyKey: string) => {
+		const returnType = Reflect.getMetadata('design:returntype', target, propertyKey);
+
+		// if (returnType === Promise) {
+		// 	getParameterTwo(clazz)
+		// }else{
+		// 	getParameterTwo(returnType)
+		// }
+	}
+}
+
+function paramOne(path:string) {
+	return (target: any, propertyKey: string, parameterIndex: number) => {
+		const paramtypes = Reflect.getMetadata('design:paramtypes', target, propertyKey);
+		methodsOne.set(target, propertyKey);
+	}
+}
+
+setTimeout(() => {
+	for(let [target, propertyKey] of methodsOne.entries()){
+		const paramtypes = Reflect.getMetadata('design:paramtypes', target, propertyKey);
+		console.log(paramtypes)
+	}
+}, 5000);
+
+function getParameterTwo(target) {
+	console.log(target.name)
+	const ref = reflect(target)
+	const obj = new target;
+	Object.getOwnPropertyNames(obj).forEach(k => {
+		const params = ref.getParameter(k)
+		console.log(k, params.type._ref)
+		const relatObj = params.type
+		if(/^class\s/.test(relatObj.ref)){
+			getParameterTwo(relatObj.class)
+		}
+	})
+}
+
+// import * as swaggerUi from "swagger-ui-express"
+// this.app.use(
+// 	"/docs",
+// 	swaggerUi.serve,
+// 	swaggerUi.setup(undefined, {
+// 	  swaggerOptions: {
+// 		url: "/api-docs.json",
+// 	  },
+// 	})
+//   );
