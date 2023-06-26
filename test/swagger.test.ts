0970260b35adf0b0979d4662fcf5fc6e3ec8276c
speedphp <jake@speedphp.com>
2023-06-26 18:18:42 +0800

    test

--- test/swagger.test.ts
+++ test/swagger.test.ts
@@ -4,26 +4,28 @@ const expect = chaiObj.expect;
 
 describe("Test Swagger plugin for TypeSpeed", () => {
     let appClose;
-    before(() => {
+    before((done) => {
         appClose = require("../app/main");
+        done();
     });
     it("Swagger UI", (done) => {
-        chaiObj.request("http://localhost:8082").get("/docs").end((err, res) => {
+        chaiObj.request("http://app:8082").get("/docs").end((err, res) => {
             expect(res.status).to.be.equal(200);
             done();
         });
     });
     it("JSDoc for Project", (done) => {
         const jsonFile = require("../app/example.json");
-        chaiObj.request("http://localhost:8082").get("/docs/swagger.json").end((err, res) => {
+        chaiObj.request("http://app:8082").get("/docs/swagger.json").end((err, res) => {
             expect(JSON.stringify(res.body)).to.be.equal(JSON.stringify(jsonFile));
             done();
         });
     });
-    after(() => {
+    after((done) => {
         if(appClose != null){
             appClose.default();
         }
+        done();
     });
 });
 
