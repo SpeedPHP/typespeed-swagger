688e07be3a59caae8fd4561b37a09f747bd4b0d7
speedphp <jake@speedphp.com>
2023-06-26 23:37:41 +0800

    读取到了再赋值，看这里可能还是需要通过 listen 回调

--- app/main.ts
+++ app/main.ts
@@ -3,22 +3,32 @@ import { swaggerMiddleware } from "../index";
 import * as path from "path";
 
 let appServer = null;
-
+let esApp = null;
 @app
 class Main {
 
     @autoware
-    public server : ServerFactory;
+    public server: ServerFactory;
 
-    public main(){
+    public main() {
         const packageJson = path.join(__dirname, "./package.json");
         swaggerMiddleware(this.server.app, null, packageJson);
         appServer = this.server.start(8082);
+        esApp = this.server.app;
     }
 }
 
-export default () => {
-    if (appServer != null) {
-        appServer.close();
-    }
-};
+function getApp() {
+    return new Promise((resolve, reject) => {
+        const loop = () => {
+            if (esApp !== null) {
+                resolve(esApp)
+            }else {
+                setTimeout(loop)
+            }
+        }
+        loop();
+    });
+}
+
+export default getApp;
--- test/swagger.test.ts
+++ test/swagger.test.ts
@@ -4,29 +4,31 @@ const expect = chaiObj.expect;
 
 describe("Test Swagger plugin for TypeSpeed", () => {
     let appClose;
-    before((done) => {
+    let esApp;
+    before(async () => {
         appClose = require("../app/main");
-        done();
+        esApp = await appClose.default();
     });
-    it("Swagger UI", (done) => {
-        chaiObj.request("http://127.0.0.1:8082").get("/docs").end((err, res) => {
+    it("Swagger UI", (done) =>  {
+        console.log(esApp)
+        chaiObj.request(esApp).get("/docs").end((err, res) => {
             expect(res.status).to.be.equal(200);
             done();
         });
     });
-    it("JSDoc for Project", (done) => {
-        const jsonFile = require("../app/example.json");
-        chaiObj.request("http://127.0.0.1:8082").get("/docs/swagger.json").end((err, res) => {
-            expect(JSON.stringify(res.body)).to.be.equal(JSON.stringify(jsonFile));
-            done();
-        });
-    });
-    after((done) => {
-        if(appClose != null){
-            appClose.default();
-        }
-        done();
-    });
+    // it("JSDoc for Project", (done) => {
+    //     const jsonFile = require("../app/example.json");
+    //     chaiObj.request("http://127.0.0.1:8082").get("/docs/swagger.json").end((err, res) => {
+    //         expect(JSON.stringify(res.body)).to.be.equal(JSON.stringify(jsonFile));
+    //         done();
+    //     });
+    // });
+    // after((done) => {
+    //     if(appClose != null){
+    //         appClose.default();
+    //     }
+    //     done();
+    // });
 });
 
 
