ef0f3141d3a92e8dffa6788a1ac34229a31ad5ef
speedphp <jake@speedphp.com>
2023-06-27 14:53:41 +0800

    增加测试

--- test/swagger.test.ts
+++ test/swagger.test.ts
@@ -6,23 +6,24 @@ const expect = chaiObj.expect;
 describe("Test Swagger plugin for TypeSpeed", () => {
     let appClose;
     const hostname = process.env.LOCAL_HOST || os.hostname();
+    const testAddr = `http://${hostname}:8082`;
     before(function () {
         this.timeout(20000);
         appClose = require("../app/main");
     });
     it("Swagger UI", (done) =>  {
-        chaiObj.request(`http://${hostname}:8082`).get("/docs/").end((err, res) => {
+        chaiObj.request(testAddr).get("/docs/").end((err, res) => {
             expect(res.status).to.be.equal(200);
             done();
         });
     });
-    // it("JSDoc for Project", (done) => {
-    //     const jsonFile = require("../app/example.json");
-    //     chaiObj.request("http://127.0.0.1:8082").get("/docs/swagger.json").end((err, res) => {
-    //         expect(JSON.stringify(res.body)).to.be.equal(JSON.stringify(jsonFile));
-    //         done();
-    //     });
-    // });
+    it("JSDoc for Project", (done) => {
+        const jsonFile = require("../app/example.json");
+        chaiObj.request(testAddr).get("/docs/swagger.json").end((err, res) => {
+            expect(JSON.stringify(res.body)).to.be.equal(JSON.stringify(jsonFile));
+            done();
+        });
+    });
     after((done) => {
         if(appClose != null){
             appClose.default();
